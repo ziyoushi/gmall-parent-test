c5c6053dd955943904022469fcdc4d9fb8db1768
changchen <doublechang123@163.com>
2019-12-09 23:02:35 +0800

    第一次项目结构初始化提交

---
+++ gmall-admin-web/src/test/java/com/zb/data/gmall/admin/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zb.data.gmall.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gmall-pms/src/main/java/com/zb/data/gmall/gmallpms/GmallPmsApplication.java
@@ -0,0 +1,13 @@
+package com.zb.data.gmall.gmallpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPmsApplication.class, args);
+    }
+
+}
